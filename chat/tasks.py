1d19f9bbd9426def6c8d9e91833c645db8ff5537
Your Name <you@example.com>
2021-02-20 19:51:10 +0100

    'setup_final_done'

--- chat/tasks.py
+++ chat/tasks.py
@@ -1,40 +1,20 @@
 from __future__ import absolute_import,unicode_literals
 
 from celery import shared_task
-
+from chat.models import Message
 
 @shared_task
 def delete_messages():
-    messages = [
-        {
-            id:1,
-            "sender":"ayoub sadi",
-            "content":"thank you sur"
-        },{
-            
-            id:2,
-            "sender":"monir sokar",
-            "content":"yes but fuk you sur"
-        },{
-            
-            id:3,
-            "sender":"ahmed sadi",
-            "content":"thanks is not bitsh"
-        },{
-            
-            id:4,
-            "sender":"moad sadi",
-            "content":"yes king thanks"
-        }
-    ]   
-    list_interdi = ["fuk","sex","bitsh",'fuker']
-    list_interdi = ["fuk","sex","bitsh",'fuker']
+    messages = Message.objects.all()
+    list_interdi = ["fuk","sexe","bitsh",'fuker']
     counter = 0
     for msg in messages:
         for i in list_interdi:
-            if i in msg['content']:
-                print(msg['content'])
+            if i in msg.content:
+                msg.delete()
                 counter +=1
+    print(messages)
     print(counter)         
+
                 
 
--- project/settings.py
+++ project/settings.py
@@ -124,5 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_RESULT_BACKEND = 'django-cache'
+
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
